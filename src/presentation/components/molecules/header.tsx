e8a5e472424bbb3c642615e1af3a5bd2528b9d0f
Decante <gustavocamposantunes@gmail.com>
2025-06-07 19:53:09 -0300

    refactor: remove items form header menu

--- src/presentation/components/molecules/header.tsx
+++ src/presentation/components/molecules/header.tsx
@@ -10,29 +10,21 @@ export const Header = () => {
   const router = useRouter()
 
   return (
-    <header className="bg-[#AD0F0A] px-16 py-4 flex flex-col md:flex-row justify-between items-center">
-      <span className='flex items-center gap-1'>
-        <Image
-          src="./logo.svg"
-          width={50}
-          height={50}
-          alt='logo fatec conecta'
-        />
-        <h1 className="text-white uppercase font-bold">Conecta</h1>
-      </span>
+    <header className="bg-[#AD0F0A] px-6 py-4 flex flex-col md:flex-row justify-between items-center">
+      <Link href="/">
+        <span className='flex items-center gap-1'>
+          <Image
+            src="./logo.svg"
+            width={50}
+            height={50}
+            alt='logo fatec conecta'
+          />
+          <h1 className="text-white uppercase font-bold">Conecta</h1>
+        </span>
+      </Link>
       <nav className="border-t-2 border-white pt-2 md:border-t-0">
         <ul className="flex items-center gap-4 text-white font-semibold">
-          <li data-testid="home-menu-item">
-            <Link href="/">
-              Início
-            </Link>
-          </li>
-          <li data-testid="projects">
-            <Link href="/project">
-              Projetos
-            </Link>
-          </li>
-          <li className="border-l-2 border-white pl-6">
+          <li className="pl-6">
             <Link href="/register-user">Registre-se</Link>
           </li>
           <li>
