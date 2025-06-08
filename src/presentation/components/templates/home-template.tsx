a2159684c4ca5512500691650062a55710b69460
Decante <gustavocamposantunes@gmail.com>
2025-06-08 14:57:18 -0300

    refactor: add another text and image for the project description

--- src/presentation/components/templates/home-template.tsx
+++ src/presentation/components/templates/home-template.tsx
@@ -1,9 +1,11 @@
 import { ActionCard } from "../organisms/action-card";
 import { Card, CardContent, CardHeader, CardTitle } from "../atoms/card";
 import { CustomCarousel } from "../organisms/custom-carousel";
+import ComunidadeFatec from '@/presentation/assets/comunidade-fatec.png';
 
 import { toast } from 'react-toastify';
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 export const HomeTemplate = () => {
   const router = useRouter();
@@ -13,16 +15,15 @@ export const HomeTemplate = () => {
   }
   return (
     <main className="p-4 flex flex-col gap-4">
-      <section>
+      <section className="flex gap-2">
         <CustomCarousel />
-      </section>
-      <section>
         <Card data-testid="main-info-fatec-conecta">
           <CardHeader>
-            <CardTitle>Como funciona o FATEC CONECTA?</CardTitle>
+            <CardTitle className="uppercase text-center">Como funciona o FATEC CONECTA?</CardTitle>
           </CardHeader>
-          <CardContent>
-            <p>Pensando na utilização de poder ajudar o sistema de produção de novas consultorias e projetos, nós da Lucky Labs decidimos investir em um projeto aberto que visa abrir para a comunidade um espaço para que possam inserir todas as suas reclamações atuais na sociedade cotidiana, também dando espaço para que alunos na FATEC possam analisar reclamações como possíveis tópicos de interesse para desenvolverem novos projetos e assim, abrir mais possibilidades da FATEC integrar-se em situações do dia-a-dia.</p>
+          <CardContent className="flex gap-1">
+            <Image src={ComunidadeFatec.src} width={150} height={100} alt="comunidade e fatec" />
+            <p className="text-xl">Conectar <b className="text-[#D9755B]">comunidade</b>, <b className="text-[#1B425E]">alunos</b> e <b className="text-[#1B425F]">coordenação</b> da <b className="text-[#AD0F0A]">Fatec Votorantim</b> o projeto <b>Fatec Conecta</b> tem como objetivo transformar as demandas da comunidade em Projetos Integradores para os cursos de Desenvolvimento de Software Multiplataforma, Ciência de Dados para Negócios e Controle de Obras.</p>
           </CardContent>
         </Card>
       </section>
